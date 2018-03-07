0676065e232097fe9c40372e8e683b488a1d7629
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-03-07 21:44:35 +0100

    Added something

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ console.log('Lemon curry...?');
 document.getElementById('app').innerText = 'Hello there!';
 
 const box = document.createElement('div');
-document.body.appendChild('div');
+document.body.appendChild(box);
